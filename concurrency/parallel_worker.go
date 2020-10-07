1216c38788c0b9d648d7557581d39442c401d610
Soulou <leo@unbekandt.eu>
2020-10-08 00:14:23 +0200

    Add package 'concurrency' with struct ParallelWorker, include doc and example as test
    FYI 'go test' execute the Example and compare the output as the comment // Output: [...]

---
+++ concurrency/parallel_worker.go
@@ -0,0 +1,47 @@
+package concurrency
+
+import "sync"
+
+// ParallelWorker is a struct providing an helper to run tasks in parallel
+// while capping the parallelism with a known number of workers.
+type ParallelWorker struct {
+	sem         chan struct{}
+	wg          *sync.WaitGroup
+	endFunction func()
+}
+
+// NewParallelWorker constructs a new parallel worker running at maximum
+// <workers> jobs at a time. <endFunc> is a callback called when all the jobs
+// are over and that "Stop" is called.
+func NewParallelWorker(workers int, endFunc func()) ParallelWorker {
+	w := ParallelWorker{
+		sem:         make(chan struct{}, workers),
+		wg:          &sync.WaitGroup{},
+		endFunction: endFunc,
+	}
+
+	return w
+}
+
+// Stop should be called to release all resources. It waits for all the runnings
+// tasks to be over Calling Perform after this would result in a panic
+func (w ParallelWorker) Stop() {
+	w.wg.Wait()
+	close(w.sem)
+	w.endFunction()
+}
+
+// Perform adds a job to handle. It can be called any number of time The
+// concurrencry will never get over the number of workers defined at the
+// initialization
+func (w ParallelWorker) Perform(function func()) {
+	w.wg.Add(1)
+	go func() {
+		w.sem <- struct{}{}
+		go func() {
+			defer w.wg.Done()
+			function()
+			<-w.sem
+		}()
+	}()
+}
