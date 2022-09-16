75e38d1b41dc2b068b95076ba9157c418d275529
Soulou <leo@scalingo.com>
2022-09-16 08:57:59 +0200

    Lint

--- crypto/hmac.go
+++ crypto/hmac.go
@@ -9,13 +9,13 @@ import (
 // HMAC512 sha512 hashes data with the given key.
 func HMAC512(key, plainText []byte) []byte {
 	mac := hmac.New(sha512.New, key)
-	_, _ = mac.Write([]byte(plainText))
+	_, _ = mac.Write(plainText)
 	return mac.Sum(nil)
 }
 
 // HMAC256 sha256 hashes data with the given key.
 func HMAC256(key, plainText []byte) []byte {
 	mac := hmac.New(sha256.New, key)
-	_, _ = mac.Write([]byte(plainText))
+	_, _ = mac.Write(plainText)
 	return mac.Sum(nil)
 }
