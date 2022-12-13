08fe7b16caff8bc414e36eef878915061f892f8e
CURZOLA Pierre <pierre.curzola@gmail.com>
2022-12-13 15:34:29 +0100

    feat(errors): add method Errorf and non formatted methods

--- errors/cause_test.go
+++ errors/cause_test.go
@@ -19,10 +19,37 @@ func Test_IsRootCause(t *testing.T) {
 		assert.False(t, IsRootCause(err, ValidationErrors{}))
 	})
 
+	t.Run("given an error stack with errgo.NoteMask", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = errgo.NoteMask(err, "biniou")
+
+		assert.True(t, IsRootCause(err, &ValidationErrors{}))
+		assert.False(t, IsRootCause(err, ValidationErrors{}))
+	})
+
+	t.Run("given an error stack with errors.Wrap", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = errors.Wrap(err, "biniou")
+
+		assert.True(t, IsRootCause(err, &ValidationErrors{}))
+		assert.False(t, IsRootCause(err, ValidationErrors{}))
+	})
+
 	t.Run("given an error stack with errors.Wrapf", func(t *testing.T) {
 		var err error
 		err = (&ValidationErrors{})
 		err = errors.Wrapf(err, "biniou")
+
+		assert.True(t, IsRootCause(err, &ValidationErrors{}))
+		assert.False(t, IsRootCause(err, ValidationErrors{}))
+	})
+
+	t.Run("given an error stack with Wrap from ErrCtx", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = Wrap(context.Background(), err, "biniou")
 
 		assert.True(t, IsRootCause(err, &ValidationErrors{}))
 		assert.False(t, IsRootCause(err, ValidationErrors{}))
@@ -36,10 +63,20 @@ func Test_IsRootCause(t *testing.T) {
 		assert.True(t, IsRootCause(err, &ValidationErrors{}))
 		assert.False(t, IsRootCause(err, ValidationErrors{}))
 	})
+
 	t.Run("given an error stack with Notef from ErrCtx", func(t *testing.T) {
 		var err error
 		err = (&ValidationErrors{})
 		err = Notef(context.Background(), err, "biniou")
+
+		assert.True(t, IsRootCause(err, &ValidationErrors{}))
+		assert.False(t, IsRootCause(err, ValidationErrors{}))
+	})
+
+	t.Run("given an error stack with NoteMask from ErrCtx", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = NoteMask(context.Background(), err, "biniou")
 
 		assert.True(t, IsRootCause(err, &ValidationErrors{}))
 		assert.False(t, IsRootCause(err, ValidationErrors{}))
@@ -71,6 +108,18 @@ func Test_RootCause(t *testing.T) {
 		assert.Equal(t, "test=biniou", RootCause(err).Error())
 	})
 
+	t.Run("given an error stack with errgo.NoteMask", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{
+			Errors: map[string][]string{
+				"test": {"biniou"},
+			},
+		})
+		err = errgo.NoteMask(err, "pouet")
+
+		assert.Equal(t, "test=biniou", RootCause(err).Error())
+	})
+
 	t.Run("given an error stack with errors.Wrap", func(t *testing.T) {
 		var err error
 		err = (&ValidationErrors{
@@ -79,6 +128,18 @@ func Test_RootCause(t *testing.T) {
 			},
 		})
 		err = errors.Wrap(err, "pouet")
+
+		assert.Equal(t, "test=biniou", RootCause(err).Error())
+	})
+
+	t.Run("given an error stack with Wrap from ErrCtx", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{
+			Errors: map[string][]string{
+				"test": {"biniou"},
+			},
+		})
+		err = Wrap(context.Background(), err, "pouet")
 
 		assert.Equal(t, "test=biniou", RootCause(err).Error())
 	})
@@ -103,6 +164,18 @@ func Test_RootCause(t *testing.T) {
 			},
 		})
 		err = Notef(context.Background(), err, "pouet")
+
+		assert.Equal(t, "test=biniou", RootCause(err).Error())
+	})
+
+	t.Run("given an error stack with NoteMask from ErrCtx", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{
+			Errors: map[string][]string{
+				"test": {"biniou"},
+			},
+		})
+		err = NoteMask(context.Background(), err, "pouet")
 
 		assert.Equal(t, "test=biniou", RootCause(err).Error())
 	})
