8b4bdc8880070b4714aadd5469213e266d0ca32b
Soulou <leo@scalingo.com>
2023-04-05 14:06:03 +0200

    feat(Is/As) Match API from standard library and leverage it to check for error value of type
    I'm proposing to deprecate IsRootCause and if(errors.RootCause(err) == something to use errors.As(err, errWithExpectedType) errors.Is(err, something)
    
    Fixes #582

--- errors/cause_test.go
+++ errors/cause_test.go
@@ -2,12 +2,142 @@ package errors
 
 import (
 	"context"
+	"io"
+	"net"
 	"testing"
 
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	errgo "gopkg.in/errgo.v1"
 )
+
+type customError struct {
+	WrappedError error
+	CustomValue  string
+}
+
+func (err *customError) Error() string {
+	return "custom error " + err.CustomValue
+}
+
+func (err *customError) Unwrap() error {
+	return err.WrappedError
+}
+
+func Test_As(t *testing.T) {
+	var expectedErrorType *ValidationErrors
+	var unexpectedErrorType *net.OpError
+	t.Run("given an error stack with errgo.Notef", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = errgo.Notef(err, "biniou")
+
+		assert.True(t, As(err, &expectedErrorType))
+		assert.False(t, As(err, &unexpectedErrorType))
+	})
+
+	t.Run("given an error stack with errors.Wrap", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = errors.Wrap(err, "biniou")
+
+		assert.True(t, As(err, &expectedErrorType))
+		assert.False(t, As(err, &unexpectedErrorType))
+	})
+
+	t.Run("given an error stack with errors.Wrapf", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = errors.Wrapf(err, "biniou")
+
+		assert.True(t, As(err, &expectedErrorType))
+		assert.False(t, As(err, &unexpectedErrorType))
+	})
+
+	t.Run("given an error stack with Wrap from ErrCtx", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = Wrap(context.Background(), err, "biniou")
+
+		assert.True(t, As(err, &expectedErrorType))
+		assert.False(t, As(err, &unexpectedErrorType))
+	})
+
+	t.Run("given an error stack with Wrapf from ErrCtx", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = Wrapf(context.Background(), err, "biniou")
+
+		assert.True(t, As(err, &expectedErrorType))
+		assert.False(t, As(err, &unexpectedErrorType))
+	})
+
+	t.Run("given an error stack with Notef from ErrCtx", func(t *testing.T) {
+		var err error
+		err = (&ValidationErrors{})
+		err = Notef(context.Background(), err, "biniou")
+
+		assert.True(t, As(err, &expectedErrorType))
+		assert.False(t, As(err, &unexpectedErrorType))
+	})
+
+	t.Run("given an error in the middle of the stack stack with Notef from ErrCtx", func(t *testing.T) {
+		var err error
+		err = io.EOF
+		err = &customError{WrappedError: err, CustomValue: "value"}
+		err = Notef(context.Background(), err, "biniou")
+
+		var expectedErrorType *customError
+		assert.True(t, As(err, &expectedErrorType))
+		assert.False(t, As(err, &unexpectedErrorType))
+	})
+}
+
+func Test_Is(t *testing.T) {
+	t.Run("given an error stack with errgo.Mask", func(t *testing.T) {
+		expectedError := io.EOF
+		err := errgo.Mask(expectedError, errgo.Any)
+
+		assert.True(t, Is(err, expectedError))
+	})
+
+	t.Run("given an error stack with errgo.Notef", func(t *testing.T) {
+		expectedError := io.EOF
+		err := errgo.Notef(expectedError, "pouet")
+
+		assert.True(t, Is(err, expectedError))
+	})
+
+	t.Run("given an error stack with errors.Wrap", func(t *testing.T) {
+		expectedError := io.EOF
+		err := errors.Wrap(expectedError, "pouet")
+
+		assert.True(t, Is(err, expectedError))
+	})
+
+	t.Run("given an error stack with Wrap from ErrCtx", func(t *testing.T) {
+		expectedError := io.EOF
+		err := Wrap(context.Background(), expectedError, "pouet")
+
+		assert.True(t, Is(err, expectedError))
+	})
+
+	t.Run("given an error stack with Wrapf from ErrCtx", func(t *testing.T) {
+		expectedError := io.EOF
+		err := Wrapf(context.Background(), expectedError, "pouet")
+
+		assert.True(t, Is(err, expectedError))
+	})
+
+	t.Run("given an error stack with mixed types", func(t *testing.T) {
+		expectedError := io.EOF
+		err := Notef(context.Background(), expectedError, "pouet")
+		err = errgo.Notef(err, "pouet")
+		err = errors.Wrap(err, "pouet")
+
+		assert.True(t, Is(err, expectedError))
+	})
+}
 
 func Test_IsRootCause(t *testing.T) {
 	t.Run("given an error stack with errgo.Notef", func(t *testing.T) {
