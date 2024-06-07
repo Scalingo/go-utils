83a0560aabba259a974d4711bd4ada1106776c2b
Cédric Thomas <cedric.thomas@scalingo.com>
2024-06-07 15:54:57 +0200

    feat: comments of the goose pgx wrapper

--- postgresql/goose/migration.go
+++ postgresql/goose/migration.go
@@ -14,10 +14,11 @@ import (
 
 type PgxContextMigration func(context.Context, *pgx.Conn) error
 
-// AddPgxContextMigration adds a migration that uses a pgx.Conn instead of a sql.DB
+// AddPgxContextMigration registers a migration in the goose migrations list using a pgx.Conn instead of a sql.DB
 // It can only be used with a pgx driver
 func AddPgxContextMigration(upMigration PgxContextMigration, downMigration PgxContextMigration) error {
 	// runtime.Caller returns the filename of the migration file that calls this function
+	// it is used to save the migration in the goose migrations list
 	_, filename, _, ok := runtime.Caller(1)
 	if !ok {
 		return errors.New(context.Background(), "could not get caller filename")
@@ -37,10 +38,16 @@ func wrapNoTxContextMigrationToPgxContextMigration(pgxContextMigration PgxContex
 		if err != nil {
 			return errors.Wrap(ctx, err, "could not get a connection from the connection pool")
 		}
-		// Raw is a function that allows to run a function with the underlying driver connection as argument
+		// Raw is a function that allows to run a function with the raw driver connection as argument
 		err = sqlConn.Raw(func(driverConn interface{}) error {
-			// pgxConn is the underlying driver connection that casted to a pgx.Conn
-			pgxConn := driverConn.(*stdlib.Conn).Conn()
+			// stdlibConn is the raw driver connection casted to a pgx stdlib connection
+			// stdlib is a package that wraps the pgx driver to make it compatible with the sql.DB interface
+			stdlibConn, ok := driverConn.(*stdlib.Conn)
+			if !ok {
+				return errors.New(ctx, "could not cast the driver connection to a pgx.Conn")
+			}
+			// pgxConn is the pgx connection extracted from the stdlib connection
+			pgxConn := stdlibConn.Conn()
 			return pgxContextMigration(ctx, pgxConn)
 		})
 		if err != nil {
