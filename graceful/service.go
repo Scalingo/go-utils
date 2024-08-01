9fb45de7b03b8666c48d51240817b05d30d7738b
Zeno Kerr <zeno.kerr@scalingo.com>
2024-08-01 16:20:24 +0200

    Allow graceful to handle multiple listeners

--- graceful/service.go
+++ graceful/service.go
@@ -3,23 +3,22 @@ package graceful
 import (
 	"context"
 	"crypto/tls"
-	"fmt"
 	"net/http"
 	"os"
 	"sync"
 	"time"
 
-	"github.com/Scalingo/go-utils/errors/v2"
-
 	"github.com/cloudflare/tableflip"
 
+	"github.com/Scalingo/go-utils/errors/v2"
 	"github.com/Scalingo/go-utils/logger"
 )
 
 type Service struct {
-	httpServer *http.Server
-	graceful   *tableflip.Upgrader
-	wg         *sync.WaitGroup
+	httpServers []*http.Server
+	graceful    *tableflip.Upgrader
+	mx          sync.Mutex
+	wg          *sync.WaitGroup
 	// waitDuration is the duration which is waited for all connections to stop
 	// in order to graceful shutdown the server. If some connections are still up
 	// after this timer they'll be cut aggressively.
@@ -28,6 +27,8 @@ type Service struct {
 	// connection to close when a graceful restart has been ordered. The new
 	// process is already working as expecting.
 	reloadWaitDuration time.Duration
+	// numServers is the number of servers to register before being ready
+	numServers int
 	// pidFile tracks the pid of the last child among the chain of graceful restart
 	// Required for daemon manager to track the service
 	pidFile string
@@ -37,9 +38,11 @@ type Option func(*Service)
 
 func NewService(opts ...Option) *Service {
 	s := &Service{
+		httpServers:        make([]*http.Server, 0),
 		wg:                 &sync.WaitGroup{},
 		waitDuration:       time.Minute,
 		reloadWaitDuration: 30 * time.Minute,
+		numServers:         1,
 	}
 	for _, opt := range opts {
 		opt(s)
@@ -65,6 +68,28 @@ func WithPIDFile(path string) Option {
 	})
 }
 
+func WithNumServers(n int) Option {
+	return Option(func(s *Service) {
+		s.numServers = n
+	})
+}
+
+func (s *Service) getTableflipUpgrader(ctx context.Context) (*tableflip.Upgrader, error) {
+	var err error
+	s.mx.Lock()
+	if s.graceful == nil {
+		s.graceful, err = tableflip.New(tableflip.Options{
+			UpgradeTimeout: s.reloadWaitDuration,
+			PIDFile:        s.pidFile,
+		})
+		if err != nil {
+			return nil, errors.Wrap(ctx, err, "creating tableflip upgrader")
+		}
+	}
+	s.mx.Unlock()
+	return s.graceful, nil
+}
+
 func (s *Service) ListenAndServeTLS(ctx context.Context, proto string, addr string, handler http.Handler, tlsConfig *tls.Config) error {
 	httpServer := &http.Server{
 		Addr:      addr,
@@ -85,27 +110,19 @@ func (s *Service) ListenAndServe(ctx context.Context, proto string, addr string,
 func (s *Service) listenAndServe(ctx context.Context, _ string, addr string, server *http.Server) error {
 	log := logger.Get(ctx)
 
-	if s.pidFile != "" {
-		pid := os.Getpid()
-		err := os.WriteFile(s.pidFile, []byte(fmt.Sprintf("%d\n", pid)), 0600)
+	if len(s.httpServers) == 0 {
+		err := s.prepare(ctx)
 		if err != nil {
-			return errors.Wrap(ctx, err, "fail to write PID file")
+			// purposefully do not wrap error here, as it is wrapped in prepare
+			return err
 		}
 	}
 
 	// Use tableflip to handle graceful restart requests
-	upg, err := tableflip.New(tableflip.Options{
-		UpgradeTimeout: s.reloadWaitDuration,
-		PIDFile:        s.pidFile,
-	})
+	upg, err := s.getTableflipUpgrader(ctx)
 	if err != nil {
-		return errors.Wrap(ctx, err, "creating tableflip upgrader")
+		return errors.Wrap(ctx, err, "get upgrader")
 	}
-	s.graceful = upg
-	defer upg.Stop()
-
-	// setup the signal handling
-	go s.setupSignals(ctx)
 
 	// Listen must be called before Ready
 	ln, err := upg.Listen("tcp", addr)
@@ -117,7 +134,7 @@ func (s *Service) listenAndServe(ctx context.Context, _ string, addr string, ser
 		ln = tls.NewListener(ln, server.TLSConfig)
 	}
 
-	s.httpServer = server
+	s.httpServers = append(s.httpServers, server)
 
 	go func() {
 		err := server.Serve(ln)
@@ -126,11 +143,48 @@ func (s *Service) listenAndServe(ctx context.Context, _ string, addr string, ser
 		}
 	}()
 
+	if len(s.httpServers) == s.numServers {
+		err := s.finalize(ctx)
+		if err != nil {
+			// purposefully do not wrap error here, as it is wrapped in finalize
+			return err
+		}
+	}
+
+	return nil
+}
+
+// prepare is called before the first server is started.
+func (s *Service) prepare(ctx context.Context) error {
+	if s.pidFile != "" {
+		err := os.Remove(s.pidFile)
+		if err != nil && !os.IsNotExist(err) {
+			return errors.Wrap(ctx, err, "fail to remove PID file")
+		}
+	}
+
+	// setup the signal handling
+	go s.setupSignals(ctx)
+
+	return nil
+}
+
+// finalize is called when all servers are started.
+func (s *Service) finalize(ctx context.Context) error {
+	log := logger.Get(ctx)
+
+	upg, err := s.getTableflipUpgrader(ctx)
+	if err != nil {
+		return errors.Wrap(ctx, err, "get upgrader")
+	}
+	defer upg.Stop()
+
 	log.Info("ready")
 	if err := upg.Ready(); err != nil {
 		return errors.Wrapf(ctx, err, "upgrader notify ready")
 	}
 	<-upg.Exit()
+	log.Info("upgrader finished")
 
 	// Normally the server should be always gracefully stopped and entering the
 	// above condition when server is closed If by any mean the serve stops
@@ -144,9 +198,30 @@ func (s *Service) listenAndServe(ctx context.Context, _ string, addr string, ser
 	}
 
 	// Wait for connections to drain.
-	err = server.Shutdown(ctx)
-	if err != nil {
-		return errors.Wrap(ctx, err, "server shutdown")
+	errChan := make(chan error, len(s.httpServers))
+	var wg sync.WaitGroup
+	for i, httpServer := range s.httpServers {
+		wg.Add(1)
+		go func(i int, httpServer *http.Server) {
+			defer wg.Done()
+			err = httpServer.Shutdown(ctx)
+			if err != nil {
+				errChan <- errors.Wrapf(ctx, err, "server shutdown %d", i)
+			}
+		}(i, httpServer)
+	}
+	wg.Wait()
+	close(errChan)
+	var shutdownErr error
+	for err := range errChan {
+		if shutdownErr == nil {
+			shutdownErr = err
+		} else {
+			shutdownErr = errors.Wrap(ctx, shutdownErr, err.Error())
+		}
+	}
+	if shutdownErr != nil {
+		return shutdownErr
 	}
 
 	return nil
@@ -175,15 +250,47 @@ func (s *Service) DecConnCount(ctx context.Context) {
 // developer.
 func (s *Service) shutdown(ctx context.Context) error {
 	log := logger.Get(ctx)
-	log.Info("shutting down http server")
-	err := s.httpServer.Shutdown(ctx)
-	if err != nil {
-		return errors.Wrapf(ctx, err, "fail to shutdown http server")
+
+	errChan := make(chan error, len(s.httpServers))
+	var wg sync.WaitGroup
+
+	for i, httpServer := range s.httpServers {
+		wg.Add(1)
+		go func(i int, httpServer *http.Server) {
+			defer wg.Done()
+			log := logger.Get(ctx)
+			if len(s.httpServers) > 1 {
+				log = log.WithField("index", i)
+			}
+			log.Info("shutting down http server")
+			err := httpServer.Shutdown(ctx)
+			if err != nil {
+				log.WithError(err).Error("fail to shutdown http server")
+				errChan <- errors.Wrapf(ctx, err, "fail to shutdown http server %d", i)
+			} else {
+				log.Info("http server is stopped")
+			}
+		}(i, httpServer)
 	}
-	log.Info("http server is stopped")
+
+	wg.Wait()
+	close(errChan)
+
+	var shutdownErr error
+	for err := range errChan {
+		if shutdownErr == nil {
+			shutdownErr = err
+		} else {
+			shutdownErr = errors.Wrap(ctx, shutdownErr, err.Error())
+		}
+	}
+
+	if shutdownErr != nil {
+		return shutdownErr
+	}
 
 	log.Info("wait hijacked connections")
-	err = s.waitHijackedConnections(ctx)
+	err := s.waitHijackedConnections(ctx)
 	if err != nil {
 		return errors.Wrapf(ctx, err, "fail to wait hijacked connections")
 	}
--- graceful/testdata/cmd/server/server.go
+++ graceful/testdata/cmd/server/server.go
@@ -2,38 +2,103 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 
+	"github.com/Scalingo/go-utils/errors/v2"
 	"github.com/Scalingo/go-utils/graceful"
 )
 
 func main() {
-	timeout := time.Minute
-	if len(os.Args) == 2 && os.Args[1] != "" {
-		timeoutI, _ := strconv.Atoi(os.Args[1])
-		timeout = time.Duration(timeoutI) * time.Millisecond
+	numListeners := 1
+
+	// default options
+	options := []graceful.Option{
+		graceful.WithWaitDuration(time.Minute),
+		graceful.WithPIDFile("./testdata/server.pid"),
 	}
+
+	// customise options
+	for _, arg := range os.Args[1:] {
+		// Split the option at the first '='
+		idx := strings.Index(arg, "=")
+		if idx == -1 {
+			continue
+		}
+		opt := arg[:idx]
+		val := arg[idx+1:]
+
+		switch opt {
+		case "pid-file":
+			options = append(options, graceful.WithPIDFile(val))
+		case "wait-duration":
+			timeoutI, _ := strconv.Atoi(val)
+			options = append(options, graceful.WithWaitDuration(time.Duration(timeoutI)*time.Millisecond))
+		case "num-servers":
+			numListeners, _ = strconv.Atoi(val)
+			options = append(options, graceful.WithNumServers(numListeners))
+		}
+	}
+
 	ctx := context.Background()
 	s := graceful.NewService(
-		graceful.WithWaitDuration(timeout),
-		graceful.WithPIDFile("./testdata/server.pid"),
+		options...,
 	)
-	router := http.NewServeMux()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		sleepStr := r.URL.Query().Get("sleep")
-		sleep, _ := strconv.Atoi(sleepStr)
-		if sleep != 0 {
-			time.Sleep(time.Duration(sleep) * time.Millisecond)
+
+	errChan := make(chan error, numListeners)
+	var wg sync.WaitGroup
+
+	for i := 0; i < numListeners; i++ {
+		wg.Add(1)
+		port := 9000 + i
+		endpoint := "/"
+		if i > 0 {
+			endpoint = fmt.Sprintf("/%d", i)
 		}
-	})
-	log.Println("Serving on :9000")
-	err := s.ListenAndServe(ctx, "tcp", ":9000", router)
-	if err != nil {
-		log.Println("I'm dead because of", err)
-		os.Exit(-1)
+
+		router := http.NewServeMux()
+		router.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+			sleepStr := r.URL.Query().Get("sleep")
+			sleep, _ := strconv.Atoi(sleepStr)
+			if sleep != 0 {
+				time.Sleep(time.Duration(sleep) * time.Millisecond)
+			}
+			w.Header().Set("Content-Type", "text/plain")
+			w.Write([]byte("Hello, world! Listener: " + endpoint))
+		})
+
+		go func(i int) {
+			defer wg.Done()
+			addr := fmt.Sprintf(":%d", port)
+			log.Printf("Serving on :%s\n", addr)
+			err := s.ListenAndServe(ctx, "tcp", addr, router)
+			if err != nil {
+				log.Println("I'm dead because of", err)
+				errChan <- errors.Wrapf(ctx, err, "I'm dead because of")
+			}
+		}(i)
 	}
+
+	wg.Wait()
+	close(errChan)
+
+	var shutdownErr error
+	for err := range errChan {
+		if shutdownErr == nil {
+			shutdownErr = err
+		} else {
+			shutdownErr = errors.Wrap(ctx, shutdownErr, err.Error())
+		}
+	}
+
+	if shutdownErr != nil {
+		log.Fatal(ctx, shutdownErr)
+	}
+
 }
