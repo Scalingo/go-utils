a702a9631edd57726a739b0610d3577dbee70cce
Brandon WELSCH <brandon@scalingo.com>
2025-02-28 11:38:48 +0100

    feat(otel): Add more sync int64 instruments to oteltest package

--- otel/oteltest/int64_instruments.go
+++ otel/oteltest/int64_instruments.go
@@ -6,6 +6,7 @@ import (
 	"go.opentelemetry.io/otel/metric"
 )
 
+// MockInt64CounterWrapper is a wrapper around a metric.Int64Counter that allows for mocking.
 type MockInt64CounterWrapper struct {
 	metric.Int64Counter
 
@@ -14,4 +15,36 @@ type MockInt64CounterWrapper struct {
 
 func (m *MockInt64CounterWrapper) Add(ctx context.Context, value int64, options ...metric.AddOption) {
 	m.counter.Add(ctx, value, options...)
+}
+
+// MockInt64GaugeWrapper is a wrapper around a metric.Int64Gauge that allows for mocking.
+type MockInt64GaugeWrapper struct {
+	metric.Int64Gauge
+
+	gauge metric.Int64Gauge
+}
+
+func (m *MockInt64GaugeWrapper) Record(ctx context.Context, value int64, options ...metric.RecordOption) {
+	m.gauge.Record(ctx, value, options...)
+}
+
+// MockInt64HistogramWrapper is a wrapper around a metric.Int64Histogram that allows for mocking.
+type MockInt64HistogramWrapper struct {
+	metric.Int64Histogram
+
+	histogram metric.Int64Histogram
+}
+
+func (m *MockInt64HistogramWrapper) Record(ctx context.Context, value int64, options ...metric.RecordOption) {
+	m.histogram.Record(ctx, value, options...)
+}
+
+type MockInt64UpDownCounterWrapper struct {
+	metric.Int64UpDownCounter
+
+	upDownCounter metric.Int64UpDownCounter
+}
+
+func (m *MockInt64UpDownCounterWrapper) Add(ctx context.Context, value int64, options ...metric.AddOption) {
+	m.upDownCounter.Add(ctx, value, options...)
 }
