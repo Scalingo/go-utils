ee9b5aa517ff6732ce3ed48a2976b2a7c7f990ee
Étienne Michon <etienne@scalingo.com>
2025-04-10 15:20:13 +0200

    build(workflows): execute the CI via GitHub Actions

--- mongo/document/document.go
+++ mongo/document/document.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/sirupsen/logrus"
-
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
@@ -69,7 +68,7 @@ var _ Validable = &Base{}
 func Create(ctx context.Context, collectionName string, doc document) error {
 	return save(ctx, collectionName, doc, func(ctx context.Context, collectionName string, doc document) error {
 		log := logger.Get(ctx)
-		//nolint contextcheck
+		//nolint: contextcheck
 		c := mongo.Session(log).Clone().DB("").C(collectionName)
 		defer c.Database.Session.Close()
 		log.WithFields(logrus.Fields{
@@ -83,7 +82,7 @@ func Create(ctx context.Context, collectionName string, doc document) error {
 func Save(ctx context.Context, collectionName string, doc document) error {
 	return save(ctx, collectionName, doc, func(ctx context.Context, collectionName string, doc document) error {
 		log := logger.Get(ctx)
-		//nolint contextcheck
+		//nolint: contextcheck
 		c := mongo.Session(log).Clone().DB("").C(collectionName)
 		defer c.Database.Session.Close()
 		log.Debugf("save '%v'", collectionName)
@@ -95,7 +94,7 @@ func Save(ctx context.Context, collectionName string, doc document) error {
 func Update(ctx context.Context, collectionName string, update bson.M, doc document) error {
 	return save(ctx, collectionName, doc, func(ctx context.Context, collectionName string, doc document) error {
 		log := logger.Get(ctx)
-		//nolint contextcheck
+		//nolint: contextcheck
 		c := mongo.Session(log).Clone().DB("").C(collectionName)
 		defer c.Database.Session.Close()
 
