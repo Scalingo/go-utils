80dc49b259a74ceb1c954def841bf2fbe8642ae8
Soulou <leo@scalingo.com>
2026-01-19 09:55:26 +0100

    CI: lint

--- errors/join_test.go
+++ errors/join_test.go
@@ -22,69 +22,77 @@ func (e *CustomError) Error() string {
 
 var ErrNotFound = errors.New("not found")
 
+type testContextKey string
+
+const (
+	testKey1    testContextKey = "testKey1"
+	testKey2    testContextKey = "testKey2"
+	testKeyRoot testContextKey = "testKeyRoot"
+)
+
 func TestJoin_RootCtxFromFirstError(t *testing.T) {
 	t.Run("It should get the root context from the first errctx error when joining 2 errctx errors", func(t *testing.T) {
 		// Given
-		ctx1 := context.WithValue(context.Background(), "key1", "value1")
-		ctx1 = context.WithValue(ctx1, "root", "first")
+		ctx1 := context.WithValue(t.Context(), testKey1, "value1")
+		ctx1 = context.WithValue(ctx1, testKeyRoot, "first")
 		err1 := New(ctx1, "first error")
 
-		ctx2 := context.WithValue(context.Background(), "key2", "value2")
-		ctx2 = context.WithValue(ctx2, "root", "second")
+		ctx2 := context.WithValue(t.Context(), testKey2, "value2")
+		ctx2 = context.WithValue(ctx2, testKeyRoot, "second")
 		err2 := New(ctx2, "second error")
 
 		// When
 		joinedErr := Join(err1, err2)
-		require.NotNil(t, joinedErr)
+		require.Error(t, joinedErr)
 
 		// Then
-		rootCtx := RootCtxOrFallback(context.Background(), joinedErr)
+		rootCtx := RootCtxOrFallback(t.Context(), joinedErr)
 		assert.NotNil(t, rootCtx)
-		assert.Equal(t, "value1", rootCtx.Value("key1"))
-		assert.Equal(t, "first", rootCtx.Value("root"))
+		assert.Equal(t, "value1", rootCtx.Value(testKey1))
+		assert.Equal(t, "first", rootCtx.Value(testKeyRoot))
 		// The second error's context should not be accessible
-		assert.Nil(t, rootCtx.Value("key2"))
+		assert.Nil(t, rootCtx.Value(testKey2))
 	})
 
 	t.Run("It should get the root context from nested errctx errors in the first error when joining", func(t *testing.T) {
 		// Given
 		// First error with nested context
-		rootCtx1 := context.WithValue(context.Background(), "root_key", "root_value")
+		rootCtx1 := context.WithValue(t.Context(), testKeyRoot, "root_value")
 		err1 := New(rootCtx1, "base error")
 
-		wrapCtx1 := context.WithValue(context.Background(), "wrap_key", "wrap_value")
+		wrapCtx1 := context.WithValue(t.Context(), testKey1, "wrap_value")
 		err1 = Wrap(wrapCtx1, err1, "wrapped error")
 
 		// Second error with different context
-		ctx2 := context.WithValue(context.Background(), "key2", "value2")
+		ctx2 := context.WithValue(t.Context(), testKey2, "value2")
 		err2 := New(ctx2, "second error")
 
 		// When
 		joinedErr := Join(err1, err2)
-		require.NotNil(t, joinedErr)
+		require.Error(t, joinedErr)
 
 		// Then - should get the root context from the first error
-		rootCtx := RootCtxOrFallback(context.Background(), joinedErr)
+		rootCtx := RootCtxOrFallback(t.Context(), joinedErr)
 		assert.NotNil(t, rootCtx)
-		assert.Equal(t, "root_value", rootCtx.Value("root_key"))
+		assert.Equal(t, "root_value", rootCtx.Value(testKeyRoot))
 		// Should not have values from wrapping or second error
-		assert.Nil(t, rootCtx.Value("wrap_key"))
-		assert.Nil(t, rootCtx.Value("key2"))
+		assert.Nil(t, rootCtx.Value(testKey1))
+		assert.Nil(t, rootCtx.Value(testKey2))
 	})
 }
 
 func TestJoin_Is(t *testing.T) {
 	t.Run("It should match error value with Is when wrapped in ErrCtx and joined", func(t *testing.T) {
 		// Given
-		ctx1 := context.WithValue(context.Background(), "key1", "value1")
+		ctx1 := context.WithValue(t.Context(), testKey1, "value1")
 		err1 := Wrap(ctx1, ErrNotFound, "wrapped not found")
 
-		ctx2 := context.WithValue(context.Background(), "key2", "value2")
+		ctx2 := context.WithValue(t.Context(), testKey2, "value2")
 		err2 := New(ctx2, "second error")
 
 		// When
 		joinedErr := Join(err1, err2)
-		require.NotNil(t, joinedErr)
+		require.Error(t, joinedErr)
 
 		// Then
 		assert.True(t, Is(joinedErr, ErrNotFound))
@@ -92,15 +100,15 @@ func TestJoin_Is(t *testing.T) {
 
 	t.Run("It should match error value with Is when in second joined error", func(t *testing.T) {
 		// Given
-		ctx1 := context.WithValue(context.Background(), "key1", "value1")
+		ctx1 := context.WithValue(t.Context(), testKey1, "value1")
 		err1 := New(ctx1, "first error")
 
-		ctx2 := context.WithValue(context.Background(), "key2", "value2")
+		ctx2 := context.WithValue(t.Context(), testKey2, "value2")
 		err2 := Wrap(ctx2, ErrNotFound, "wrapped not found in second")
 
 		// When
 		joinedErr := Join(err1, err2)
-		require.NotNil(t, joinedErr)
+		require.Error(t, joinedErr)
 
 		// Then
 		assert.True(t, Is(joinedErr, ErrNotFound))
@@ -111,19 +119,19 @@ func TestJoin_Is(t *testing.T) {
 		ErrDatabase := errors.New("database error")
 		ErrNetwork := errors.New("network error")
 
-		ctx1 := context.WithValue(context.Background(), "key1", "value1")
+		ctx1 := context.WithValue(t.Context(), testKey1, "value1")
 		err1 := Wrap(ctx1, ErrDatabase, "wrapped database error")
 
-		ctx2 := context.WithValue(context.Background(), "key2", "value2")
+		ctx2 := context.WithValue(t.Context(), testKey2, "value2")
 		err2 := Wrap(ctx2, ErrNetwork, "wrapped network error")
 
 		// When
 		joinedErr := Join(err1, err2)
-		require.NotNil(t, joinedErr)
+		require.Error(t, joinedErr)
 
 		// Then
-		assert.True(t, errors.Is(joinedErr, ErrDatabase))
-		assert.True(t, errors.Is(joinedErr, ErrNetwork))
+		require.ErrorIs(t, joinedErr, ErrDatabase)
+		require.ErrorIs(t, joinedErr, ErrNetwork)
 	})
 }
 
@@ -131,15 +139,15 @@ func TestJoin_As(t *testing.T) {
 	t.Run("It should match error value with As when wrapped in ErrCtx and joined", func(t *testing.T) {
 		// Given
 		customErr := &CustomError{Code: 404, Message: "resource not found"}
-		ctx1 := context.WithValue(context.Background(), "key1", "value1")
+		ctx1 := context.WithValue(t.Context(), testKey1, "value1")
 		err1 := Wrap(ctx1, customErr, "wrapped custom error")
 
-		ctx2 := context.WithValue(context.Background(), "key2", "value2")
+		ctx2 := context.WithValue(t.Context(), testKey2, "value2")
 		err2 := New(ctx2, "second error")
 
 		// When
 		joinedErr := Join(err1, err2)
-		require.NotNil(t, joinedErr)
+		require.Error(t, joinedErr)
 
 		// Then
 		var target *CustomError
@@ -151,16 +159,16 @@ func TestJoin_As(t *testing.T) {
 
 	t.Run("It should match error value with As when in second joined error", func(t *testing.T) {
 		// Given
-		ctx1 := context.WithValue(context.Background(), "key1", "value1")
+		ctx1 := context.WithValue(t.Context(), testKey1, "value1")
 		err1 := New(ctx1, "first error")
 
 		customErr := &CustomError{Code: 500, Message: "internal error"}
-		ctx2 := context.WithValue(context.Background(), "key2", "value2")
+		ctx2 := context.WithValue(t.Context(), testKey2, "value2")
 		err2 := Wrap(ctx2, customErr, "wrapped custom error in second")
 
 		// When
 		joinedErr := Join(err1, err2)
-		require.NotNil(t, joinedErr)
+		require.Error(t, joinedErr)
 
 		// Then
 		var target *CustomError
