576bc43602c7fe787569777237cc13f91cfe4994
Étienne Michon <etienne@scalingo.com>
2026-03-02 16:55:06 +0100

    build(deps): update `github.com/urfave/cli` from v1 to v3

--- gomock_generator/go.mod
+++ gomock_generator/go.mod
@@ -9,12 +9,11 @@ require (
 	github.com/Scalingo/go-utils/logger v1.12.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.4
-	github.com/urfave/cli v1.22.17
+	github.com/urfave/cli/v3 v3.7.0
 )
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
--- gomock_generator/main.go
+++ gomock_generator/main.go
@@ -9,52 +9,47 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v3"
 
 	"github.com/Scalingo/go-utils/gomock_generator/gomockgenerator"
 	"github.com/Scalingo/go-utils/logger"
 )
 
-var version = "1.2.2"
-
-type app struct {
-	config gomockgenerator.GenerationConfiguration
-	cli    *cli.App
-}
+var version = "1.4.1"
 
 func main() {
-	cli.AppHelpTemplate = fmt.Sprintf(`%s
-EXAMPLE:
+	cli.RootCommandHelpTemplate = fmt.Sprintf(`%s
+	EXAMPLE:
 
-%s --concurrent-goroutine 8 --mocks-filename mymocks.json --signatures-filename sigs.json
+	%s --concurrent-goroutine 8 --mocks-filename mymocks.json --signatures-filename sigs.json
 
-Reads the mymocks.json file from the current directory and generates the mocks, 8 goroutines at a time. The signatures of the mocks are stored in sigs.json, in the folder designated by the base package written in mymocks.json.
+	Reads the mymocks.json file from the current directory and generates the mocks, 8 goroutines at a time. The signatures of the mocks are stored in sigs.json, in the folder designated by the base package written in mymocks.json.
 
-`, cli.AppHelpTemplate, os.Args[0])
+	`, cli.RootCommandHelpTemplate, os.Args[0])
 
-	app := app{
-		config: gomockgenerator.GenerationConfiguration{},
-		cli:    cli.NewApp(),
-	}
-	app.cli.Name = "GoMock generator"
-	app.cli.Usage = "Highly parallelized generator of gomock mocks"
-	app.cli.Version = version
-	app.cli.Flags = []cli.Flag{
-		cli.StringFlag{Name: "mocks-filepath", Value: "./mocks.json", Usage: "Path to the JSON file containing the MockConfiguration. Location of this file is the base package.", EnvVar: "MOCKS_FILEPATH"},
-		cli.StringFlag{Name: "signatures-filename", Value: "mocks_sig.json", Usage: "Filename of the signatures cache. Location of this file is the base package.", EnvVar: "SIGNATURES_FILENAME"},
-		cli.IntFlag{Name: "concurrent-goroutines", Value: 4, Usage: "Concurrent amount of goroutines to generate mock.", EnvVar: "CONCURRENT_GOROUTINES"},
-		cli.BoolFlag{Name: "debug", Usage: "Activate debug logs"},
-	}
-	cli.AppHelpTemplate = `NAME:
+	cfgGeneration := gomockgenerator.GenerationConfiguration{}
+
+	cmd := cli.Command{
+		Name:      "GoMock generator",
+		UsageText: "gomock_generator",
+		Usage:     "Highly parallelized generator of gomock mocks",
+		Version:   version,
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "mocks-filepath", Value: "./mocks.json", Usage: "Path to the JSON file containing the MockConfiguration. Location of this file is the base package.", Sources: cli.EnvVars("MOCKS_FILEPATH")},
+			&cli.StringFlag{Name: "signatures-filename", Value: "mocks_sig.json", Usage: "Filename of the signatures cache. Location of this file is the base package.", Sources: cli.EnvVars("SIGNATURES_FILENAME")},
+			&cli.IntFlag{Name: "concurrent-goroutines", Value: 4, Usage: "Concurrent amount of goroutines to generate mock.", Sources: cli.EnvVars("CONCURRENT_GOROUTINES")},
+			&cli.BoolFlag{Name: "debug", Usage: "Activate debug logs"},
+		},
+		CustomRootCommandHelpTemplate: `NAME:
    {{.Name}} - {{.Usage}}
 USAGE:
-   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}
+   {{.UsageText}} {{if .VisibleFlags}}[global options]{{end}}
    {{if len .Authors}}
 AUTHOR:
    {{range .Authors}}{{ . }}{{end}}
    {{end}}{{if .Commands}}
 COMMANDS:
-{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+{{range .Commands}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{if .VisibleFlags}}
 GLOBAL OPTIONS:
    {{range .VisibleFlags}}{{.}}
    {{end}}{{end}}{{if .Copyright }}
@@ -64,47 +59,48 @@ COPYRIGHT:
 VERSION:
    {{.Version}}
    {{end}}
-`
-	app.cli.Before = func(c *cli.Context) error {
-		app.config.MocksFilePath = c.GlobalString("mocks-filepath")
-		app.config.SignaturesFilename = c.GlobalString("signatures-filename")
-		app.config.ConcurrentGoroutines = c.GlobalInt("concurrent-goroutines")
-		return nil
+`,
+		Before: func(ctx context.Context, cmd *cli.Command) (context.Context, error) {
+			cfgGeneration.MocksFilePath = cmd.String("mocks-filepath")
+			cfgGeneration.SignaturesFilename = cmd.String("signatures-filename")
+			cfgGeneration.ConcurrentGoroutines = cmd.Int("concurrent-goroutines")
+			return ctx, nil
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			err := validateBinaryDeps()
+			if err != nil {
+				return err
+			}
+
+			log := logger.Default()
+			if cmd.Bool("debug") {
+				log = logger.Default(logger.WithLogLevel(logrus.DebugLevel))
+			}
+			ctx = logger.ToCtx(ctx, log)
+
+			log.WithFields(logrus.Fields{
+				"mocks_file_path":       cfgGeneration.MocksFilePath,
+				"signatures_filename":   cfgGeneration.SignaturesFilename,
+				"concurrent_goroutines": cfgGeneration.ConcurrentGoroutines,
+			}).Info("Configuration for this mocks generation")
+
+			rawFile, err := os.Open(cfgGeneration.MocksFilePath)
+			if err != nil {
+				return errors.Wrap(err, "fail to open the mocks file")
+			}
+			defer rawFile.Close()
+
+			mocksConfiguration := gomockgenerator.MocksConfiguration{}
+			err = json.NewDecoder(rawFile).Decode(&mocksConfiguration)
+			if err != nil {
+				return errors.Wrap(err, "mocks file does not contain valid JSON")
+			}
+
+			return gomockgenerator.GenerateMocks(ctx, cfgGeneration, mocksConfiguration)
+		},
 	}
-	app.cli.Action = func(c *cli.Context) error {
-		err := validateBinaryDeps()
-		if err != nil {
-			return err
-		}
 
-		log := logger.Default()
-		if c.GlobalBool("debug") {
-			log = logger.Default(logger.WithLogLevel(logrus.DebugLevel))
-		}
-		ctx := logger.ToCtx(context.Background(), log)
-
-		log.WithFields(logrus.Fields{
-			"mocks_file_path":       app.config.MocksFilePath,
-			"signatures_filename":   app.config.SignaturesFilename,
-			"concurrent_goroutines": app.config.ConcurrentGoroutines,
-		}).Info("Configuration for this mocks generation")
-
-		rawFile, err := os.Open(app.config.MocksFilePath)
-		if err != nil {
-			return errors.Wrap(err, "fail to open the mocks file")
-		}
-		defer rawFile.Close()
-
-		mocksConfiguration := gomockgenerator.MocksConfiguration{}
-		err = json.NewDecoder(rawFile).Decode(&mocksConfiguration)
-		if err != nil {
-			return errors.Wrap(err, "mocks file does not contain valid JSON")
-		}
-
-		return gomockgenerator.GenerateMocks(ctx, app.config, mocksConfiguration)
-	}
-
-	err := app.cli.Run(os.Args)
+	err := cmd.Run(context.Background(), os.Args)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
